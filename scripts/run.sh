0281a05f2bb9b6f492dfa6863ee2d172272da8b1
ohnoohweh <owackerl@gmx.net>
2026-03-25 08:45:44 +0300

    Fix restart loop exit logic in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,10 @@
 # Den gewünschten Befehl hier definieren
 COMMAND="./bin/python3 -m obstacle_bridge -c obstacle_bridge.cfg"
 
-until [ $? -eq 75 ]; do
+while true; do
   $COMMAND > /dev/null
+  rc=$?
+  if [ "$rc" -ne 75 ]; then
+    exit "$rc"
+  fi
 done
