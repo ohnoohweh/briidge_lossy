fa4c271c65ad5c62a6b70cae17b17d8232b3fe96
ohnoohweh <owackerl@gmx.net>
2026-09-16 13:58:04 +0200

    build: include R006 Core sources in iOS project

--- ios/scripts/patch_ios_xcode_project.py
+++ ios/scripts/patch_ios_xcode_project.py
@@ -95,6 +95,8 @@ IPSERVER_SHARED_SWIFT_SOURCES = [
     ("71C500000000000000000032", "71C500000000000000000132", "ObstacleBridgeControlChunkCodec.swift"),
     ("71C500000000000000000033", "71C500000000000000000133", "ObstacleBridgeServiceCodec.swift"),
     ("71C500000000000000000011", "71C500000000000000000111", "ObstacleBridgeWebSocketPayloadCodec.swift"),
+    ("71C50000000000000000003A", "71C50000000000000000013A", "ObstacleBridgeOverlayCoordinator.swift"),
+    ("71C50000000000000000003B", "71C50000000000000000013B", "ObstacleBridgeOverlayEnvelope.swift"),
     ("71C500000000000000000012", "71C500000000000000000112", "ObstacleBridgeWebSocketOverlayRuntime.swift"),
     ("71C500000000000000000025", "71C500000000000000000125", "ObstacleBridgeWebSocketOverlayTransportOwner.swift"),
     ("71C500000000000000000013", "71C500000000000000000113", "ObstacleBridgeTcpOverlayRuntime.swift"),
@@ -141,6 +143,8 @@ APP_SHARED_SWIFT_SOURCES = [
     ("71C61000000000000000000C", "71C61000000000000000010C", "ObstacleBridgeCompressLayerRuntime.swift"),
     ("71C610000000000000000027", "71C610000000000000000127", "ObstacleBridgeOverlayStackPlanner.swift"),
     ("71C61000000000000000000D", "71C61000000000000000010D", "ObstacleBridgeWebSocketPayloadCodec.swift"),
+    ("71C61000000000000000003A", "71C61000000000000000013A", "ObstacleBridgeOverlayCoordinator.swift"),
+    ("71C61000000000000000003B", "71C61000000000000000013B", "ObstacleBridgeOverlayEnvelope.swift"),
     ("71C610000000000000000030", "71C610000000000000000130", "ObstacleBridgeBinaryCodec.swift"),
     ("71C610000000000000000038", "71C610000000000000000138", "ObstacleBridgeCore.swift"),
     ("71C610000000000000000037", "71C610000000000000000137", "ObstacleBridgeSecureLinkPSKTranscript.swift"),
@@ -180,7 +184,7 @@ CORE_SWIFT_SOURCE_ROOT = "../../../../../swift/Sources/ObstacleBridgeCore"
 
 
 def shared_swift_source_path(name: str) -> str:
-    if name in {"ObstacleBridgeCore.swift", "ObstacleBridgeSecureLinkPSKTranscript.swift", "ObstacleBridgeWebSocketPayloadCodec.swift", "ObstacleBridgeBinaryCodec.swift", "ObstacleBridgeChannelMuxFrameCodec.swift", "ObstacleBridgeCompression.swift", "ObstacleBridgeMyUDPCodec.swift", "ObstacleBridgeSecureLinkFrameCodec.swift", "ObstacleBridgeOverlayFrameCodec.swift", "ObstacleBridgeControlChunkCodec.swift", "ObstacleBridgeServiceCodec.swift"}:
+    if name in {"ObstacleBridgeCore.swift", "ObstacleBridgeSecureLinkPSKTranscript.swift", "ObstacleBridgeWebSocketPayloadCodec.swift", "ObstacleBridgeOverlayCoordinator.swift", "ObstacleBridgeOverlayEnvelope.swift", "ObstacleBridgeBinaryCodec.swift", "ObstacleBridgeChannelMuxFrameCodec.swift", "ObstacleBridgeCompression.swift", "ObstacleBridgeMyUDPCodec.swift", "ObstacleBridgeSecureLinkFrameCodec.swift", "ObstacleBridgeOverlayFrameCodec.swift", "ObstacleBridgeControlChunkCodec.swift", "ObstacleBridgeServiceCodec.swift"}:
         return f"{CORE_SWIFT_SOURCE_ROOT}/{name}"
     return f"../../../../native/ObstacleBridgeShared/{name}"
 
